d743194e40a46e796d8ec8111371612689015b40
Andrey Maximenko <andrey.v.maximenko@gmail.com>
2023-05-16 11:27:42 +0300

    change decor

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,13 +2,13 @@ package ru.netology.service;
 
 public class CashbackHackServiceTest {
 
-   @org.testng.annotations.Test
+    @org.testng.annotations.Test
     public void shouldBuyMoreWhenAmountBelowBoundary() { //покупка меньше 1000р
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int expected = 500;
         int actual = service.remain(amount);
-       org.testng.Assert.assertEquals(actual, expected);
+        org.testng.Assert.assertEquals(actual, expected);
     }
 
     @org.testng.annotations.Test
@@ -47,7 +47,6 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         org.testng.Assert.assertEquals(actual, expected);
     }
-
 
 
 }
